aab6f1eefbf05d623216bba56091df7f206828ed
Gustavo-Edgar <guguedgar@gmail.com>
2025-03-07 23:55:37 +0000

    Terminei o Desafio Novato

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main(){
+    printf("Movimento da Torre:\n");
+    for(int Torre = 0; Torre < 5; Torre++){
+        printf("Direita\n");
+    }
+
+    printf("\nMovimento do Bispo:\n");
+    for(int Bispo = 0; Bispo < 5; Bispo++){
+        printf("Cima Direita\n");
+    }
+
+    printf("\nMovimento da Rainha:\n");
+    for(int Rainha = 0; Rainha < 8; Rainha++){
+        printf("Esquerda\n");
+    }
+}
