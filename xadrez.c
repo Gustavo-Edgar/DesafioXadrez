34ec9aeda93e14679c3f8278ba42af57e7c5e907
Gustavo-Edgar <guguedgar@gmail.com>
2025-03-08 23:59:53 +0000

    Desafio Nivel Mestre Feito

--- xadrez.c
+++ xadrez.c
@@ -1,28 +1,47 @@
 #include<stdio.h>
 
+void Torre(int t){
+    if(t>0){
+        printf("Direita\n");
+        Torre(t-1);
+    }
+}
+
+void Bispo(int b){
+    for(b=0;b<5;b++){
+        printf("Cima ");
+        for(int b2=b-1;b2<b;b2++){
+            printf("Direita\n");
+        }
+    }
+}
+
+void Rainha(int r){
+    if(r>0){
+        printf("Esquerda\n");
+        Rainha(r-1);
+    }
+}
+
+void Cavalo(){
+    for(int C1 =0,C2=0;C1<1 && C2<2; C2++){
+        printf("Baixo ");
+        if(C2==1){
+            printf("Esquerda\n");
+            C1++;
+        }
+    }
+}
 int main(){
     printf("Movimento da Torre:\n");
-    for(int Torre = 0; Torre < 5; Torre++){
-        printf("Direita\n");
-    }
+    Torre(5);
 
     printf("\nMovimento do Bispo:\n");
-    for(int Bispo = 0; Bispo < 5; Bispo++){
-        printf("Cima Direita\n");
-    }
+    Bispo(5);
 
     printf("\nMovimento da Rainha:\n");
-    for(int Rainha = 0; Rainha < 8; Rainha++){
-        printf("Esquerda\n");
-    }
+    Rainha(8);
+
     printf("\nMovimento do Cavalo:\n");
-    for(int Cavalo1 = 0; Cavalo1<1;Cavalo1++){
-        int Cavalo2 = 0 ;
-        while(Cavalo2<2){
-            printf("Baixo\n");
-            Cavalo2++;
-        }
-        printf("Esquerda\n");
-    }
-            
+    Cavalo();           
 }
